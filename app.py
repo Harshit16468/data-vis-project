8a320be8beb751014d6243108837c3adb9e4ec90
Harshit16468 <harshit.gupta@students.iiit.ac.in>
2024-04-23 07:48:16 +0530

    final

--- app.py
+++ app.py
@@ -22,22 +22,24 @@ def create_plot(x_values, y_values, names, title, y_title, colors):
 
 app = Flask(__name__)  # Creating a Flask application instance (redundant line)
 
-csv_data = pd.read_csv("data.csv")  # Reading data from a CSV file
+csv_data = pd.read_csv("./static/data.csv")  # Reading data from a CSV file
 
 with open("country_info.json", "r") as f:  # Opening a JSON file
     country_info = json.load(f)  # Loading the JSON data
 
 @app.route('/')  # Defining a route for the root URL
 def basic():
-    return render_template('i.html')  # Rendering the 'i.html' template
-
+    return render_template('land.html')  # Rendering the 'i.html' template
+@app.route('/vis')  # Defining a route for the root URL
+def basicf():
+    return render_template('vis.html')  # Rendering the 'i.html' template
 @app.route('/watch_temperature_video', methods=['POST'])  # Defining a route for '/watch_temperature_video' with POST method
 def watch_temperature_video():
     return render_template('temp.html')  # Rendering the 'temp.html' template
 
 @app.route('/watch_sea_level_video', methods=['GET', 'POST'])  # Defining a route for '/watch_sea_level_video' with GET and POST methods
 def watch_sea_level_video():
-    return render_template('sea_level.html')  # Rendering the 'sea_level.html' template
+    return render_template('g.html')  # Rendering the 'sea_level.html' template
 
 @app.route('/render_chart_html', methods=['POST'])  # Defining a route for '/render_chart_html' with POST method
 def index():
@@ -102,9 +104,15 @@ def extent():
         'mode': 'lines+markers',  # Changed mode to include markers
         'name': 'Temperature'
     }  # Creating a trace (line plot with markers) for temperatures
+    trace3 = {
+        'x': temperatures,
+        'y': extents,
+        'mode': 'lines+markers',
+        'name': 'Third Data'
+    }
 
     chart_data = {
-        'data': [trace1, trace2],
+        'data': [trace1, trace2,trace3],
         'layout': [{
             'title': '',
             'xaxis': {'title': 'Year'},
@@ -114,6 +122,11 @@ def extent():
             'title': '',
             'xaxis': {'title': 'Year'},
             'yaxis': {'title': 'Temperature'}
+        },
+        {
+            'title': '',
+            'xaxis': {'title': 'Temperature'},
+            'yaxis': {'title': 'Extent'}
         }]
     }  # Creating a dictionary to hold the chart data (traces and layout)
 
@@ -121,59 +134,23 @@ def extent():
 
 @app.route('/heat', methods=['POST'])  # Defining a route for '/heat' with POST method
 def heat():
-    return render_template('heat.html')  # Rendering the 'heat.html' template
+    return render_template('chorloplet.html')  # Rendering the 'heat.html' template
 
-@app.route('/india-area', methods=['POST', 'GET'])  # Defining a route for '/india-area' with POST and GET methods
+@app.route('/bar-race', methods=['POST', 'GET'])  # Defining a route for '/india-area' with POST and GET methods
 def area():
-    # Read data from CSV files
-    df1 = pd.read_csv('TEMP_ANNUAL_SEASONAL_MEAN.csv')  # Reading data from a CSV file
-    df2 = pd.read_csv('RF_AI_1901-2021.csv')  # Reading data from another CSV file
-
-    # Define names and colors for each line
-    names1 = ['ANNUAL', 'JAN-FEB', 'MAR-MAY', 'JUN-SEP', 'OCT-DEC']  # List of names for the first plot
-    colors1 = ['#0000FF', '#00FF00', '#FF0000', '#FFA500', '#000000']  # List of colors for the first plot
-    names2 = ['JUN', 'JUL', 'AUG', 'SEP', 'JUN-SEP']  # List of names for the second plot
-    colors2 = ['#0000FF', '#00FF00', '#FF0000', '#FFA500', '#000000']  # List of colors for the second plot
-    names3 = ['ANNUAL', 'JAN-FEB', 'MAR-MAY', 'JUN-SEP', 'OCT-DEC']  # List of names for the third plot
-    colors3 = ['#333333', '#666666', '#999999', '#CCCCCC', '#DDDDDD']  # List of colors (grayscale) for the third plot
-    names4 = ['JUN', 'JUL', 'AUG', 'SEP', 'JUN-SEP']  # List of names for the fourth plot
-    colors4 = ['#333333', '#666666', '#999999', '#CCCCCC', '#DDDDDD']  # List of colors (grayscale) for the fourth plot
-
-    # Create plots
-    plot1 = create_plot(df1['YEAR'],
-                        [df1['ANNUAL'], df1['JAN-FEB'], df1['MAR-MAY'], df1['JUN-SEP'], df1['OCT-DEC']],
-                        names1,
-                        'Temperature (Colored)',
-                        'Temperature(in degree celcius)',
-                        colors1)  # Creating the first plot (colored temperature)
-    plot2 = create_plot(df2['YEAR'],
-                        [df2['JUN'], df2['JUL'], df2['AUG'], df2['SEP'], df2['JUN-SEP']],
-                        names2,
-                        'Rainfall (Colored)',
-                        'Rainfall in mm',
-                        colors2)  # Creating the second plot (colored rainfall)
-    plot3 = create_plot(df1['YEAR'],
-                        [df1['ANNUAL'], df1['JAN-FEB'], df1['MAR-MAY'], df1['JUN-SEP'], df1['OCT-DEC']],
-                        names3,
-                        'Temperature (Grayscale)',
-                        'Temperature(in degree celcius)',
-                        colors3)  # Creating the third plot (grayscale temperature)
-    plot4 = create_plot(df2['YEAR'],
-                        [df2['JUN'], df2['JUL'], df2['AUG'], df2['SEP'], df2['JUN-SEP']],
-                        names4,
-                        'Rainfall (Grayscale)',
-                        'Rainfall in mm',
-                        colors4)  # Creating the fourth plot (grayscale rainfall)
-
-    # Convert plots to HTML
-    plot1_div = plot1.to_html(full_html=False)  # Converting the first plot to HTML
-    plot2_div = plot2.to_html(full_html=False)  # Converting the second plot to HTML
-    plot3_div = plot3.to_html(full_html=False)  # Converting the third plot to HTML
-    plot4_div = plot4.to_html(full_html=False)  # Converting the fourth plot to HTML
-
     # Render the template with the plots
-    return render_template('hello.html', plot1_div=plot1_div, plot2_div=plot2_div, plot3_div=plot3_div,
-                           plot4_div=plot4_div)  # Rendering the 'hello.html' template and passing the plots as HTML
-
+    return render_template('bar-race.html')  # Rendering the 'hello.html' template and passing the plots as HTML
+@app.route('/sunburst', methods=['POST', 'GET'])  # Defining a route for '/india-area' with POST and GET methods
+def sunburst():
+    # Render the template with the plots
+    return render_template('h.html')  # Rendering the 'h.html' template and passing the plots as HTML
+@app.route('/bubble', methods=['POST', 'GET'])  # Defining a route for '/india-area' with POST and GET methods
+def bubble():
+    # Render the template with the plots
+    return render_template('k.html')  # Rendering the 'h.html' template and passing the plots as HTML
+@app.route('/temp', methods=['POST', 'GET'])  # Defining a route for '/india-area' with POST and GET methods
+def temp():
+    # Render the template with the plots
+    return render_template('j.html')  # Rendering the 'h.html' template and passing the plots as HTML
 if __name__ == '__main__':
     app.run(debug=True)  # Running the Flask application in debug mode
